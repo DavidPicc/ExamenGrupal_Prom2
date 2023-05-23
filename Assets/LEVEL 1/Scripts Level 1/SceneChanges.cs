2da2d73855e311865eae0e5b2dfb0ffa5180ae6a
DiegoSValderrama <105023447+DiegoSValderrama@users.noreply.github.com>
2023-05-22 20:44:09 -0500

    Diego Valderrama (Alumno 3)
    Se acabo el alumno 3

--- Assets/LEVEL 1/Scripts Level 1/SceneChanges.cs
+++ Assets/LEVEL 1/Scripts Level 1/SceneChanges.cs
@@ -7,28 +7,47 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanges : MonoBehaviour
 {
-    public Image slider;
-    private float timer;
-    public float delay = 20f;
     public string sceneName; 
+    public float delay = 20f; 
+    public Image timeBar; 
+   
+
+    private float currentTime = 0f;
+    private float totalTime = 0f; 
 
     void Start()
     {
-        Invoke("ChangeScene", delay);
-        slider.fillAmount = timer / delay;
+        totalTime = delay;
 
+     
+        UpdateTime();
     }
 
-    private void Update()
+    void Update()
     {
+        currentTime += Time.deltaTime;
+
        
+        UpdateTime();
+
+        if (currentTime >= delay)
+        {
+            ChangeScene();
+        }
+    }
+
+    void UpdateTime()
+    {
+        float remainingTime = delay - currentTime;
+
+        float fillAmount = currentTime / totalTime;
+        timeBar.fillAmount = fillAmount;
     }
 
     void ChangeScene()
     {
         SceneManager.LoadScene(sceneName);
     }
-
 
 
 
