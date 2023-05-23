0e59f9790be8df1a901510a58c8b380f1e299046
DavidPicc <dafer_pic@hotmail.com>
2023-05-22 19:40:09 -0500

    David Piccone - Nivel 3
    - Player
    - Enemigo 3
    - Disparo del enemigo 3
    - UI

---
+++ Assets/IObserver_3.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IObserver_3
+{
+    void UpdateBulletCount(int count);
+}
---
+++ Assets/Level3Manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Level3Manager : MonoBehaviour
+{
+    public static Level3Manager instance;
+    [SerializeField] public int bulletsLeft;
+    [SerializeField] TextMeshProUGUI bulletsText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        UpdateBulletText();
+    }
+
+    public void UpdateBulletText()
+    {
+        bulletsText.text = "Bullets: " + bulletsLeft.ToString();
+    }
+}
